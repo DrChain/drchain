9c7729bb39db05fd261c99b92c11177cf06b1eee
wubm <bingmu@gmail.com>
2017-06-10 19:14:22 +0800

    modify setting

--- install.sh
+++ install.sh
@@ -19,4 +19,3 @@ fi
 mkdir -p $DIR_PATH/log/ethereum $DIR_PATH/data/ethereum \
          $DIR_PATH/data/ipfs/staging $DIR_PATH/data/ipfs/data \
          $DIR_PATH/data/explorer/mondgo $DIR_PATH/log/explorer
-
--- src/drchain-server/docker/docker-entrypoint.sh
+++ src/drchain-server/docker/docker-entrypoint.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-CONF_FILE="/etc/vchain/configure.conf"
-ARG="$1"
+CONF_FILE="/etc/mres/configure.conf"
+MRES_DIR="/root/mres"
 
 change_config() {
-    cd $EXPLORER_DIR
+    cd $MRES_DIR
+    cp config.json.default config.json
+    sed -i "s/<ethernet_host>/$ETHEREUM_HOST/g" config.json
+    sed -i "s/<ethernet_port>/$ETHEREUM_RPC_PORT/g" config.json
+    sed -i "s/<ipfs_host>/$IPFS_HOST/g" config.json
+    sed -i "s/<ipfs_port>/$IPFS_PORT/g" config.json
+    sed -i "s/<ipfs_protocol>/$IPFS_PROTOCOL/g" config.json
 }
 
 main () {
-    cd $EXPLORER_DIR
+    cd $MRES_DIR
     if [ -f /initialization ] && [ `cat /initialization` == "1" ] ; then
         npm start
         exec /sbin/init
     else
-        
+        change_config
+        npm run build
         echo "1" > /initialization
     fi
 }
